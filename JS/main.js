b0d7175e443bc8575248fc880663a40192e4e24e
Fef97 <federicakr97@gmail.com>
2023-04-27 15:25:37 +0200

    commenti

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+// CALCOLO DEL PREZZO DEL BIGLIETTO DEL TRENO
+
+// chiede all'utente quabti chilometri vuole percorrere
+
+// chiedere all'utente l'età
+
+// prezzo del biglietto (0.21€ al km)
+
+// verificare la fascia di età
+
+// se ha dai 17 anni in giù, applicare sconto del 20%
+
+// se ha dai 65 anni in su, applicare sconto del 40%
+
+// ALTRIMENTI NESSUNO SCONTO
